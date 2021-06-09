55089213224dc606ecf7cd3660f4acbff2619e31
Lino Cóndor <lino.condor.123@gmail.com>
2021-06-09 08:07:38 -0500

    first commit

---
+++ Data_Types.py
@@ -0,0 +1,26 @@
+#Basic Data Types
+
+#String
+print("Hello"[0])  #Subscript
+print("Hello"[4])  #Subscript
+
+print("123" + "456")
+
+#Integer
+
+print(123 + 456)
+
+print(123_456_789)  #Read as a number but easy to understando to humans
+
+#float
+
+
+3.1451
+
+#Boolean
+
+True
+False
+
+
+
